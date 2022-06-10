438c9e3c4a6e2beaa0fc72e5e06a1cac3828632d
Przemek587 <72383546+Przemek587@users.noreply.github.com>
2022-06-10 10:27:40 +0200

    add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import ToDo from '../assets/ToDo.png'
 import Weather from '../assets/weather.png'
 import Pizza from '../assets/pizza.png'
 import Rps from '../assets/rps.png'
+import Book from '../assets/book.png'
 
 const Projects = () => {
   return (
@@ -155,6 +156,26 @@ const Projects = () => {
                   </button>
                 </a>
                 <a href='https://github.com/PrzemekBochnia/rockpaperscissors'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-800 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${Book})`}}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:scale-110'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Apliccation
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://mybookshelf-7dadf4.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-800 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/PrzemekBochnia/myBookshelf'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-800 font-bold text-lg'>
                     Code
                   </button>
