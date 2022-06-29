e2562c758f26c488aed9c039b5b1a9ef93d4c394
Przemek587 <72383546+Przemek587@users.noreply.github.com>
2022-06-29 15:41:46 +0200

    add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,7 @@ import Weather from '../assets/weather.png'
 import Pizza from '../assets/pizza.png'
 import Rps from '../assets/rps.png'
 import Book from '../assets/book.png'
+import Netflix from '../assets/netflix.png'
 
 const Projects = () => {
   return (
@@ -176,6 +177,26 @@ const Projects = () => {
                   </button>
                 </a>
                 <a href='https://github.com/PrzemekBochnia/myBookshelf'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-800 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${Netflix})`}}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:scale-110'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Apliccation
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://pb-netflix-clone.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-800 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/PrzemekBochnia/Netflix-clone'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-800 font-bold text-lg'>
                     Code
                   </button>
